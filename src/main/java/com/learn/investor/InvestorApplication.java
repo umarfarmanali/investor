7079e49eddc66307e5c320676fc9be0e6abdfe41
Umar Ali <umar.ali@anz.com>
2022-04-15 23:52:43 +1000

    Added my project

---
+++ src/main/java/com/learn/investor/InvestorApplication.java
@@ -0,0 +1,13 @@
+package com.learn.investor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestorApplication.class, args);
+	}
+
+}
